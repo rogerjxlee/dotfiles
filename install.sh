f458059d71aec1f2edc3533a38e07d3025488f29
Roger Lee <rogerjxlee@gmail.com>
2017-05-20 20:58:43 -0700

    add gitignore and git submodule

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ PWD=$(pwd)
 ( rm -f ~/.bashrc && ln -s $PWD/bash/bashrc ~/.bashrc) &&
 echo "Linked bash config!"
 
+( rm -rf ~/.config/base16-shell && ln -s $PWD/config/base16-shell ~/.config/base16-shell ) &&
+echo "Linked base16-shell config!"
+
 ( rm -f ~/.vimrc && ln -s $PWD/vim/vimrc ~/.vimrc ) &&
 ( rm -rf ~/.vim && ln -s $PWD/vim ~/.vim ) &&
 echo "Linked vim config!" ||
