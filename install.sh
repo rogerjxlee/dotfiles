206f182ffc3470e37eaedbbedc027f5f4fcc633c
Roger Lee <rogerjxlee@gmail.com>
2017-05-23 01:40:54 -0700

    lines under 80 chars

--- install.sh
+++ install.sh
@@ -2,14 +2,27 @@
 
 PWD=$(pwd)
 
-( rm -f ~/.bash_profile && ln -s $PWD/bash/bash_profile ~/.bash_profile) &&
-( rm -f ~/.bashrc && ln -s $PWD/bash/bashrc ~/.bashrc) &&
+link_all_files_in_dir () {
+  src=$1
+  dst=$2
+  for file in $src/*; do
+    basename=$(basename $file)
+    rm -f $dst/$basename && ln -s $src/$basename $dst
+  done
+}
+
+rm -f ~/.bash_profile && ln -s $PWD/bash/bash_profile ~/.bash_profile
+rm -f ~/.bashrc && ln -s $PWD/bash/bashrc ~/.bashrc
 echo "Linked bash config!"
 
-( rm -rf ~/.config/base16-shell && ln -s $PWD/config/base16-shell ~/.config/base16-shell ) &&
-echo "Linked base16-shell config!"
+rm -rf ~/.config/base16-shell &&
+ln -s $PWD/config/base16-shell ~/.config/base16-shell &&
+echo "Linked base16-shell config!" &&
 
-( rm -f ~/.vimrc && ln -s $PWD/vim/vimrc ~/.vimrc ) &&
-( rm -rf ~/.vim && ln -s $PWD/vim ~/.vim ) &&
-echo "Linked vim config!" ||
+rm -f ~/.vimrc && ln -s $PWD/vim/vimrc ~/.vimrc &&
+rm -rf ~/.vim && ln -s $PWD/vim ~/.vim &&
+echo "Linked vim config!" &&
+
+link_all_files_in_dir $PWD/atom ~/.atom &&
+echo "Linked atom config!" ||
 echo "Install failed! :("
